b7411709e6cc5b70e0dc62307c4aa9e418ed9efe
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-06-13 15:27:25 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,10 @@
+num1=int(input())
+num2=int(input())
+num3=num1*(num2%10)
+num4=num1*(int(num2%100/10))
+num5=num1*(int(num2/100))
+num6=num3+num4*10+num5*100
+print(num3)
+print(num4)
+print(num5)
+print(num6)
