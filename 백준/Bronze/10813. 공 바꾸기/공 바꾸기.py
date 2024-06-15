336803c62fe7525a1672cbe5fcd5120bb7b3de22
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-06-16 02:24:08 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,10 @@
+N,M=map(int,input().split(' '))
+arr=list(range(1,N+1))
+
+for _ in range(M):
+    i,j=map(int,input().split(' '))
+    k=arr[i-1]
+    arr[i-1]=arr[j-1]
+    arr[j-1]=k
+
+print(*arr)
