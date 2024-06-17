c43faa31e6ac8b3b6b279c9f2230058d3f806cc4
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-06-17 22:31:37 +0900

    Update 상수.py (슬라이싱 잘 이용하면 쉬움)

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,3 +1,4 @@
+# 슬라이싱 이용해 짠 코드
 c,d=input().split(' ')
 new_c=c[::-1]
 new_d=d[::-1]
@@ -5,3 +6,14 @@ if(new_c>new_d):
     print(new_c)
 else:
     print(new_d)
+
+# 처음 짠 코드
+'''
+a,b=input().split(' ')
+new_a=a[2]+a[1]+a[0]
+new_b=b[2]+b[1]+b[0]
+if(new_a>new_b):
+    print(new_a)
+else:
+    print(new_b)
+'''
