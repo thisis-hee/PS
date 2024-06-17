8a658d279e37d34ba33867a5027e708ba5782299
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-06-17 22:30:48 +0900

    [Bronze II] Title: 상수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,7 +1,7 @@
-a,b=input().split(' ')
-new_a=a[2]+a[1]+a[0]
-new_b=b[2]+b[1]+b[0]
-if(new_a>new_b):
-    print(new_a)
+c,d=input().split(' ')
+new_c=c[::-1]
+new_d=d[::-1]
+if(new_c>new_d):
+    print(new_c)
 else:
-    print(new_b)
+    print(new_d)
