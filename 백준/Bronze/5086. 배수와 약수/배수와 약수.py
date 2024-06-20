1a4e7ce18d69906afac0a6a1b208ada473ca2d77
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-06-20 23:59:07 +0900

    [Bronze III] Title: 배수와 약수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -0,0 +1,11 @@
+while(True):
+    a,b=map(int,input().split(' '))
+    if(a!=0 and b!=0):
+        if(b%a==0):
+            print("factor")
+        elif(a%b==0):
+            print("multiple")
+        elif(a%b!=0 and b%a!=0):
+            print("neither")
+    else:
+        break
