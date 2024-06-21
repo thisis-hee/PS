89ee952a14de6dc94bf94f1e782c565bb6bdd795
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-06-21 15:30:36 +0900

    Update 대지.py ( 왜 안됐는지 모름 )

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -1,3 +1,22 @@
+'''
+N=int(input())
+x=[]
+y=[]
+
+for _ in range(N):
+    a,b=input().split(' ')
+    if(N==1):
+        print(0,end='')
+        exit(0)
+    else:
+        x.append(a)
+        y.append(b)
+
+print((int(max(x))-int(min(x)))*(int(max(y))-int(min(y))),end='')
+'''
+
+# 코드 뭔 차이지 ?
+
 N=int(input())
 x=[]
 y=[]
