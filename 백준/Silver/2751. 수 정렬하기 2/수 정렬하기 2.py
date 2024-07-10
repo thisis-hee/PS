3e23f2c2b65b5ba8e48695d5fe5e1e14d7482a3b
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-07-10 23:35:03 +0900

    Update 수 정렬하기 2.py ( input(), sys.stdin.readline() 차이 )

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -2,6 +2,10 @@ import sys
 
 N=int(input())
 numbers=list()
+
+# 수 정렬하기 1과 같이 int(input())으로 받으면 시간 초과됨.
+# import sys 후, sys.stdin.readline()으로 받아야 시간초과가 안된다.
+
 for i in range(0,N):
     numbers.append(int(sys.stdin.readline()))
 
