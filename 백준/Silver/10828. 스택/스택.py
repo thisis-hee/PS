b3e63f0fdcb1802904c030cdfcc6b02202afe3d2
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-07-11 15:40:13 +0900

    Update 스택.py ( 시간초과 sys, split() / split(' ') 차이 )

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,10 +1,13 @@
 import sys
 
+# 시간초과 때문에 sys.stdin.readline()으로 입력받음.
 N=int(sys.stdin.readline())
 stack=[]
 
 for _ in range(N):
+    # split(' ')로 하면 출력이 안됨. 변수에 \n이 추가돼서 그럼. 
     s=sys.stdin.readline().split()
+    # 하나만 입력받아고 s만 쓰면 안되고 s[0] 붙여줘야 함
     if(s[0]=='push'):
         stack.append(s[1])
     elif(s[0]=='pop'):
