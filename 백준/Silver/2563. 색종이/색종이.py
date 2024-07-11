a0527a86e89f92a8c69b8157695907af94a2535e
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-07-11 10:57:56 +0900

    Update 색종이.py ( 2차원 배열 0으로 초기화, 2차원 배열 sum() 안됨 )

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -2,6 +2,8 @@ N=int(input())
 row=100
 col=100
 count=0
+
+# 0으로 채워진 2차원 배열 생성
 arr = [[0 for j in range(col)] for i in range(row)]
 
 for i in range(N):
@@ -10,6 +12,7 @@ for i in range(N):
         for j in range(b, b+10):
             arr[i][j]=1
 
+# 2차원 배열은 sum(arr) 안됨. 그래서 for문으로 진행
 for i in range(row):
     for j in range(col):
         if(arr[i][j]==1):
