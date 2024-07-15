e0a9a8f87d33a063dbd498ac629199ae292a2fa3
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-07-15 22:34:37 +0900

    Update 소수 구하기.py ( 에라토스테네스의 체 )

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,9 +1,11 @@
+# 에라토스테네스의 체 활용
+
 M,N=map(int,input().split())
 
 for i in range(M,N+1):
     if(i==1):
         continue
-
+    # 제곱근까지만 확인하면 됨
     for j in range(2, int(i**(1/2))+1):
         if(i%j==0):
             break
