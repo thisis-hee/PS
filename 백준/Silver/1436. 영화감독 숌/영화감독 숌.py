230bc765f855858825e0ee2640870412092ea928
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-07-16 13:28:51 +0900

    Update 영화감독 숌.py ( 숫자 늘려가며 count 늘리기 )

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,11 +1,14 @@
 N=int(input())
 
+# 666부터 숫자를 하나씩 늘려감.
 number=666
 count=0
 
 while(True):
+    # 그리고 숫자에 666이 포함되면 count를 1 늘림
     if('666' in str(number)):
         count+=1
+    # count와 입력한 N이 동일해지면 그 숫자를 출력함.
     if(count==N):
         print(number)
         break
