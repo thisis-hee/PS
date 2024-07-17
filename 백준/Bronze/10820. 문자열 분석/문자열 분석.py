d52c3b9f32a677d504bc105933fb15a96763a489
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-07-17 18:42:12 +0900

    [Bronze II] Title: 문자열 분석, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.py
@@ -0,0 +1,20 @@
+while(True):
+    try:
+        String=input()
+        lower=0
+        upper=0
+        number=0
+        space=0
+        for i in range(len(String)):
+            if(ord(String[i])==32):
+                space+=1
+            elif(ord(String[i])>=65 and ord(String[i])<=90):
+                upper+=1
+            elif(ord(String[i])>=97 and ord(String[i])<=122):
+                lower+=1
+            elif(ord(String[i])>=48 and ord(String[i])<=57):
+                number+=1
+        print(lower, upper, number, space, end=' ')
+        print()
+    except EOFError:
+        break
