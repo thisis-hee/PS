7990d5b017010aae74aef0ace6b8d021d9556850
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-07-18 23:25:51 +0900

    [Bronze I] Title: ROT13, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,19 @@
+S=input()
+
+A=list(S)
+new_list=[]
+
+for i in range(len(A)):
+    if(ord(A[i])>=65 and ord(A[i])<=77):
+        new_list.append(chr(ord(A[i])+13))
+    elif(ord(A[i])>=78 and ord(A[i])<=90):
+        new_list.append(chr(ord(A[i])-13))
+    elif(ord(A[i])>=97 and ord(A[i])<=109):
+        new_list.append(chr(ord(A[i])+13))
+    elif(ord(A[i])>=110 and ord(A[i])<=122):
+        new_list.append(chr(ord(A[i])-13))
+    else:
+        new_list.append(A[i])
+
+for i in range(len(new_list)):
+    print(new_list[i], end='')
