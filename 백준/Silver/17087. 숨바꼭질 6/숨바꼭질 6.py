0c4762df43b2b01a2d001584f2125f0a5d8989b9
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-07-20 14:13:32 +0900

    Update 숨바꼭질 6.py ( math를 활용한 최대공약수 문제 )

--- 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.py
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.py
@@ -1,3 +1,6 @@
+# 최대공약수 문제인 것을 인지한 것은 잘했음
+
+# math 라이브러리 이용해서 쉽게 풀이 가능
 import math
 
 N,S = map(int,input().split())
@@ -7,5 +10,6 @@ for i in range(len(K)):
     D.append(abs(S-K[i]))
 ans = D[0]
 for i in D:
+    # 최대공약수 구해주는 함수
     ans = math.gcd(i,ans)
 print(ans)
