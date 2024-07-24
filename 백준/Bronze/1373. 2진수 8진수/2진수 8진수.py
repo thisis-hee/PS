399d994ed774f9d6045bde8f675356d0c1647f5d
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-07-24 23:34:08 +0900

    [Bronze I] Title: 2진수 8진수, Time: 568 ms, Memory: 75280 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.py
@@ -0,0 +1,22 @@
+N=input()
+
+N=list(N)
+while(True):
+    if(len(N)%3!=0):
+        N.insert(0,'0')
+        continue
+    else:
+        break
+
+A=list(map(int,N))
+
+eight=[]
+for i in range(len(A)//3):
+    A[3*i]=A[3*i]*4
+    A[3*i+1]=A[3*i+1]*2
+    A[3*i+2]=A[3*i+2]*1
+    sum=A[3*i]+A[3*i+1]+A[3*i+2]
+    eight.append(sum)
+
+eight_string=list(map(str,eight))
+print(''.join(eight_string))
