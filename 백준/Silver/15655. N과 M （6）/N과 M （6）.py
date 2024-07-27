21c29e2df26dc313af3655af1d12030102be08cf
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-07-27 16:10:22 +0900

    Update N과 M （6）.py ( func 매개변수 설정 )

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -3,6 +3,7 @@ s=[]
 A=list(map(int,input().split()))
 A.sort()
 
+# 매개변수 설정
 def func(start):
     if(len(s)==M):
         print(' '.join(map(str,s)))
@@ -11,6 +12,7 @@ def func(start):
         for i in range(start,len(A)):
             if A[i] not in s:
                 s.append(A[i])
+                # i+1로 들어가는 이유
                 func(i+1)
                 s.pop()
 
