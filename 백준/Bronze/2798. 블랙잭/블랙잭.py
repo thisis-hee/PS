9431c1eb60603de9d45294146df8ad8fdfb14d80
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-08-06 21:48:06 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,31 @@
+N,M=map(int,input().split())
+A=list(map(int,input().split()))
+
+result=0
+for i in range(N):
+    for j in range(i+1,N):
+        for k in range(j+1,N):
+            sum=A[i]+A[j]+A[k]
+            if(sum>M):
+                continue
+            else:
+                result=max(result,sum)
+                
+print(result)
+
+
+'''
+from itertools import combinations
+
+N,M=map(int,input().split())
+A=list(map(int,input().split()))
+s=[]
+
+for i in combinations(A, 3):
+    if(sum(i)>M):
+        continue
+    else:
+        s.append(sum(i))
+
+print(max(s))
+'''
