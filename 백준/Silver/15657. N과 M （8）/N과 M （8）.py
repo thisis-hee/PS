9ccf9196e691dfbdcf2eba3ae2d9a7bddb0fd88b
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-08-08 15:23:33 +0900

    [Silver III] Title: N과 M (8), Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,16 @@
+N,M=map(int,input().split())
+A=list(map(int,input().split()))
+A.sort()
+s=[]
+
+def func(start):
+    if(len(s)==M):
+        print(*s)
+        return
+    else:
+        for i in range(start, N):
+            s.append(A[i])
+            func(i)
+            s.pop()
+            
+func(0)
