8e0515d9a90326562d25b51aa21017d6fbe4e7bf
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-08-17 22:51:49 +0900

    [Bronze I] Title: 단어 공부, Time: 68 ms, Memory: 33076 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,12 @@
+word = input().upper()
+word_list = list(set(word))
+word_cnt=[]
+
+for i in word_list:
+    count=word.count(i)
+    word_cnt.append(count)
+
+if(word_cnt.count(max(word_cnt))>=2):
+    print("?")
+else:
+    print(word_list[(word_cnt.index(max(word_cnt)))])
