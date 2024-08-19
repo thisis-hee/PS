59513cd607b395054e12faa258e7ce1fc57de674
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-08-19 21:25:10 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 304 ms, Memory: 60524 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -0,0 +1,23 @@
+import sys
+
+N,M=map(int,input().split())
+
+word_list=list()
+
+for _ in range(N):
+    a=sys.stdin.readline().rstrip()
+    if(len(a)>=M):
+        word_list.append(a)
+
+word_dict={}
+
+for word in word_list:
+    if(word not in word_dict.keys()):
+        word_dict[word]=1
+    else:
+        word_dict[word]+=1
+
+word_dict = sorted(word_dict.items(), key = lambda x : (-x[1], -len(x[0]), x[0]))
+
+for i in range(len(word_dict)):
+    print(word_dict[i][0])
