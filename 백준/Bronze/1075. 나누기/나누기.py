7f62eb7ad0c79908c4f6cad644cca67013181c7f
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-08-21 01:57:13 +0900

    [Bronze II] Title: 나누기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1075. 나누기/나누기.py
@@ -0,0 +1,16 @@
+N=int(input())
+F=int(input())
+
+new_num=list(str(N))
+new_num[-1]='0'
+new_num[-2]='0'
+a=''.join(new_num)
+a=int(a)
+
+while(True):
+    if(a%F==0):
+        a=str(a)
+        print(a[-2]+a[-1])
+        break
+    else:
+        a+=1
