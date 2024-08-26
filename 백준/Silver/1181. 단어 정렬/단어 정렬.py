0f085cdffb2b8675b08162d39c31ccba4c44993a
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-08-26 16:00:08 +0900

    [Silver V] Title: 단어 정렬, Time: 532 ms, Memory: 35732 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,13 @@
+N=int(input())
+
+word_list=[]
+
+for _ in range(N):
+    word_list.append(input())
+
+word_list=list(set(word_list))
+word_list.sort()
+word_list.sort(key=len)
+
+for i in word_list:
+    print(i)
