4256d89f24bd3313b5e91edb16f01f5b8067d180
이건희 <143998370+gunheee-leee@users.noreply.github.com>
2024-10-12 13:36:16 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 136 ms, Memory: 44,020 KB -BaekjoonHub

---
+++ Python/SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
@@ -0,0 +1,16 @@
+T=int(input())
+
+for i in range(1,T+1):
+    N=int(input())
+    num_dict={}
+    num_list=list(map(int,input().split()))
+    
+    for j in range(len(num_list)):
+        if(num_list[j] not in num_dict.keys()):
+            num_dict[num_list[j]]=1
+        else:
+            num_dict[num_list[j]]+=1
+    
+    max_key = max(num_dict, key=num_dict.get)
+    
+    print(f'#{i} {max_key}')
