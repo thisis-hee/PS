f102dccdb1aa6f08a1444a30e3c1877bc9aa7194
이건희 <143998370+thisis-hee@users.noreply.github.com>
2024-10-17 14:48:33 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 159 ms, Memory: 49,800 KB -BaekjoonHub

---
+++ Python/SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.py
@@ -0,0 +1,26 @@
+for i in range(1,11):
+    # 원본 암호문 길이
+    length=int(input())
+    
+    # 원본 암호문 입력
+    password=list(map(int,input().split()))
+
+    # 명령어 수
+    N=int(input())
+
+    # 전체 명령어
+    order=list(input().split())
+
+    for j in range(len(order)):
+        if(order[j]=='I'):
+            for k in range(int(order[j+2])):
+                password.insert(int(order[j+1])+k, int(order[j+3+k]))
+        
+        if(order[j]=='D'):
+            del password[int(order[j+1])-1:int(order[j+1])+int(order[j+2])-1]
+
+        if(order[j]=='A'):
+            for l in range(int(order[j+1])):
+                password.append(order[j+2+l])
+    
+    print(f'#{i}', *password[0:10])
