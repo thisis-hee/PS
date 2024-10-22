3badbe9fd88680ae0a8e27a77ea0755630e6a3ff
이건희 <143998370+thisis-hee@users.noreply.github.com>
2024-10-22 20:05:28 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 956 ms, Memory: 105940 KB -BaekjoonHub

---
+++ Python/백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,17 @@
+import sys
+input=sys.stdin.readline
+N,M=map(int,input().split())
+
+matrix=[list(map(int,input().split())) for _ in range(N)]
+
+sum_matrix=[[0]*(N+1) for _ in range(N+1)]
+
+for i in range(1,N+1):
+    for j in range(1,N+1):
+        sum_matrix[i][j]=sum_matrix[i-1][j]+sum_matrix[i][j-1]-sum_matrix[i-1][j-1]+matrix[i-1][j-1]
+
+
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = sum_matrix[x2][y2] - sum_matrix[x1-1][y2] - sum_matrix[x2][y1-1] + sum_matrix[x1-1][y1-1]
+    print(result)
