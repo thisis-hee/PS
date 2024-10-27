982b72b69354a057194ad742dfb75118f0415489
이건희 <143998370+thisis-hee@users.noreply.github.com>
2024-10-27 22:55:34 +0900

    [Bronze I] Title: 세로읽기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,21 @@
+word=[]
+for _ in range(5):
+    word.append(list(input()))
+
+max_row=0
+for j in range(len(word)):
+    if(len(word[j])>max_row):
+        max_row=len(word[j])
+
+add_word=[[0]*max_row for _ in range(5)]
+
+for row in range(len(word)):
+    for col in range(len(word[row])):
+        add_word[row][col]=word[row][col]
+
+add_word_t=list(map(list,zip(*add_word)))
+
+for row in range(len(add_word_t)):
+    for col in range(len(add_word_t[row])):
+        if(add_word_t[row][col]!=0):
+            print(add_word_t[row][col],end='')
