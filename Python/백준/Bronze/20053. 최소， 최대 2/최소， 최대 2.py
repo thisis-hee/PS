6bf72da3fe46efda8a8fa8ffab294e83b1a630d4
이건희 <143998370+thisis-hee@users.noreply.github.com>
2024-11-01 14:24:38 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 944 ms, Memory: 185564 KB -BaekjoonHub

---
+++ Python/백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+T=int(input())
+
+for _ in range(T):
+    N=int(input())
+    num_list=list(map(int,input().split()))
+    print(min(num_list), max(num_list))
