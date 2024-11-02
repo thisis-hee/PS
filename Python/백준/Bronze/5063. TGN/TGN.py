bc5626b15b89efd80a8603d92e895c3f1970661f
이건희 <143998370+thisis-hee@users.noreply.github.com>
2024-11-02 20:12:49 +0900

    [Bronze III] Title: TGN, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N=int(input())
+
+for _ in range(N):
+    r,e,c=map(int,input().split())
+    if(r>e-c):
+        print('do not advertise')
+    elif(r==e-c):
+        print('does not matter')
+    else:
+        print('advertise')
