9d2fe382d0c6840929f5f591c8ea25ac3bbc6c66
이건희 <143998370+thisis-hee@users.noreply.github.com>
2024-11-05 23:31:20 +0900

    Update 덩치.py ( 메모리 참조 )

--- Python/백준/Silver/7568. 덩치/덩치.py
+++ Python/백준/Silver/7568. 덩치/덩치.py
@@ -13,6 +13,7 @@ for _ in range(N):
 ans=[]
 for i in range(N):
     count=1
+    # [:] 붙여줘야 다른 객체가 됨. 안붙이면 같은 메모리를 참조해 del을 하면 weight, height에서도 지워짐
     compare_w=weight[:]
     compare_h=height[:]
     del compare_w[i]
