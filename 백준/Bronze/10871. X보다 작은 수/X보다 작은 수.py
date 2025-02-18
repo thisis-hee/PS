6bea64bf3ffdd1ecb8b43490b2d3cb3178073cbf
이건희 <143998370+thisis-hee@users.noreply.github.com>
2025-02-18 18:12:47 +0900

    [Bronze V] Title: X보다 작은 수, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,8 +1,13 @@
-N,X=map(int,input().split(' '))
-arr=list(map(int, input().split(' ')))
-new_arr=list()
-for i in range(N):
-    if(arr[i]<X):
-        new_arr.append(arr[i])
-# 배열 앞에 * 붙이면 띄어쓰기로 구분해서 출력해줌    
-print(*new_arr)
+import sys
+input=sys.stdin.readline
+
+N,X=map(int,input().split())
+
+num_list=list(map(int,input().split()))
+
+new_list=[]
+for i in range(len(num_list)):
+    if(num_list[i]<X):
+        new_list.append(num_list[i])
+
+print(*new_list)
