f321f0411251c2f01c90ca6a672b271b0944e65a
이건희 <143998370+thisis-hee@users.noreply.github.com>
2025-06-06 17:57:10 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,10 @@
+select 
+  board_id, writer_id, title, price, 
+  (case 
+     when status='DONE' then '거래완료'
+     when status='SALE' then '판매중'
+     when status='RESERVED' then '예약중'
+  end) as status
+from used_goods_board
+where created_date='2022-10-05'
+order by board_id desc
