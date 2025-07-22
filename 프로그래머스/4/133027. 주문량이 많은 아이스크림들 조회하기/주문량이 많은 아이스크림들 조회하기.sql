8e4953da07d49fe19a1553c706ac07a45db59f10
이건희 <143998370+thisis-hee@users.noreply.github.com>
2025-07-22 13:18:24 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,10 +1,15 @@
 with cte1 as (
-    select * from first_half
-    union
-    select * from july
+    select shipment_id, flavor, sum(total_order) as total_order from july
+    group by flavor
+), 
+cte2 as (
+    select 
+      F.*, F.total_order+cte1.total_order as total_sum 
+    from first_half as F
+    join cte1 on F.flavor=cte1.flavor
+    order by total_sum desc
 )
 
-select flavor from cte1
-group by flavor
-order by sum(total_order) desc
+select flavor from cte2
+order by total_sum desc
 limit 3
