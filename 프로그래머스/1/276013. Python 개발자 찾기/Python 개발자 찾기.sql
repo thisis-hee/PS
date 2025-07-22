afd1d5d22e0e965296eb44dd1e651bdb02f20137
이건희 <143998370+thisis-hee@users.noreply.github.com>
2025-07-22 10:16:18 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,3 +1,5 @@
-select id, email, first_name, last_name from developer_infos
+select
+  id, email, first_name, last_name
+from developer_infos
 where skill_1='python' or skill_2='python' or skill_3='python'
-order by id asc
+order by id
