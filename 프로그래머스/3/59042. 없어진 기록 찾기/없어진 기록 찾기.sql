76da521c5ebba16ea7ff2d1a4e1079e163d156f5
이건희 <143998370+thisis-hee@users.noreply.github.com>
2025-07-22 14:43:27 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,3 +1,4 @@
-select outs.animal_id, outs.name from animal_outs as outs
-left join animal_ins as ins on outs.animal_id=ins.animal_id
-where ins.animal_id is null
+select O.animal_id, O.name from animal_outs as O
+left outer join animal_ins as I on O.animal_id=I.animal_id
+where I.name is null and O.name is not null
+order by I.animal_id, I.name
