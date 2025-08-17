e2a0251990ac01c8cbb27924f1ab03a224acd6c7
이건희 <143998370+thisis-hee@users.noreply.github.com>
2025-08-17 14:39:59 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 384 ms, Memory: 41368 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,32 +1,40 @@
 import sys
-sys.setrecursionlimit(1000000)
-input=sys.stdin.readline
+from collections import deque
 
-def dfs(ci,cj):
-    global cnt
-    v[ci][cj]=1
-    if(arr[ci][cj]=='P'):
-        cnt+=1
+input = sys.stdin.readline
+N, M = map(int, input().split())
+arr = []
 
-    for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
-        ni,nj=ci+di,cj+dj
-        if(0<=ni<n and 0<=nj<m and arr[ni][nj]!='X' and v[ni][nj]==0):
-            dfs(ni,nj)
+for _ in range(N):
+    arr.append(list(input().rstrip()))
 
+v = [[0] * M for _ in range(N)]
 
-n,m=map(int,input().split())
+def bfs(si, sj):
+    q = deque()
+    q.append((si, sj))
+    v[si][sj] = 1
+    cnt = 0
 
-arr=list(list(input().rstrip()) for _ in range(n))
-v=[[0]*m for _ in range(n)]
-cnt=0
+    while q:
+        ci, cj = q.popleft()
 
-for i in range(len(arr)):
-    for j in range(len(arr[0])):
-        if(arr[i][j]=='I'):
-            dfs(i,j)
+        if arr[ci][cj] == 'P':
+            cnt += 1
 
+        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            ni, nj = ci + di, cj + dj
+            if 0 <= ni < N and 0 <= nj < M and arr[ni][nj] != 'X' and v[ni][nj] == 0:
+                q.append((ni, nj))
+                v[ni][nj] = 1
+    return cnt
 
-if(cnt!=0):
-    print(cnt)
-else:
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == 'I':
+            answer=bfs(i,j)
+
+if answer == 0:
     print('TT')
+else:
+    print(answer)
