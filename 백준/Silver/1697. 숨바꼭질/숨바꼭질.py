008e856cc9b1428a9690e463560c3bb42970f191
이건희 <143998370+thisis-hee@users.noreply.github.com>
2025-08-17 16:25:20 +0900

    [Silver I] Title: 숨바꼭질, Time: 96 ms, Memory: 38156 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,23 +1,30 @@
 import sys
-sys.setrecursionlimit(1000000)
-input=sys.stdin.readline
+from collections import deque
 
-N,K=map(int,input().split())
+# N -> K로 이동
+# 뒤로 한 칸, 앞으로 한 칸, 앞으로 2배 이동 가능
+# 동생은 0~100000에 위치해있음
 
-def bfs(s,e):
-    q=[]
+input = sys.stdin.readline
+N, K = map(int, input().split())
+v = [0] * 100001
+
+
+def bfs(s):
+    q = deque()
     q.append(s)
-    v=[0]*100001
-    v[s]=1
+    v[s] = 1
 
     while q:
-        c=q.pop(0)
-        if(c==e):
-            return v[c]-1
+        c = q.popleft()
 
-        for n in (c-1,c+1,2*c):
-            if(0<=n<=100000 and v[n]==0):
+        if c == K:
+            return v[c] - 1
+
+        for n in (c - 1, c + 1, 2 * c):
+            if 0 <= n <= 100000 and v[n] == 0:
                 q.append(n)
-                v[n]=v[c]+1
+                v[n] = v[c] + 1
 
-print(bfs(N,K))
+
+print(bfs(N))
