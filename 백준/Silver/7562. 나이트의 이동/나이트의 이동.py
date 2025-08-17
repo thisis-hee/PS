409974338b955f80f5c0ed8222c1987bbddaab2b
이건희 <143998370+thisis-hee@users.noreply.github.com>
2025-08-17 15:02:06 +0900

    [Silver I] Title: 나이트의 이동, Time: 1096 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,31 +1,32 @@
 import sys
-sys.setrecursionlimit(1000000)
-input=sys.stdin.readline
+from collections import deque
 
-T=int(input())
+input = sys.stdin.readline
+T = int(input())
 
-def bfs(si,sj, ei, ej):
 
-    q=[]
-    q.append((si,sj))
-    v[si][sj]=1
+def bfs(si, sj):
+    q = deque()
+    v = [[0] * N for _ in range(N)]
+    q.append((si, sj))
+    v[si][sj] = 1
 
     while q:
-        ci,cj=q.pop(0)
+        ci, cj = q.popleft()
 
-        if((ci,cj)==(ei,ej)):
-            return v[ci][cj]-1
+        if ((ci, cj) == (ei, ej)):
+            return v[ci][cj]
 
-        for di,dj in ((-2,-1),(2,-1),(2,1),(-2,1),(1,2),(1,-2),(-1,2),(-1,-2)):
-            ni,nj=ci+di,cj+dj
-            if(0<=ni<l and 0<=nj<l and v[ni][nj]==0):
-                q.append((ni,nj))
-                v[ni][nj]=v[ci][cj]+1
+        for di, dj in ((-1, -2), (-1, 2), (-2, 1), (-2, -1), (1, -2), (1, 2), (2, 1), (2, -1)):
+            ni, nj = ci + di, cj + dj
+            if (0 <= ni < N and 0 <= nj < N and v[ni][nj] == 0):
+                q.append((ni, nj))
+                v[ni][nj] = v[ci][cj] + 1
+
 
 for _ in range(T):
-    l=int(input())
-    si,sj=map(int,input().split())
-    ei,ej=map(int,input().split())
-    arr=[[0]*l for _ in range(l)]
-    v=[[0]*l for _ in range(l)]
-    print(bfs(si,sj,ei,ej))
+    N = int(input())
+    si, sj = map(int, input().split())
+    ei, ej = map(int, input().split())
+
+    print(bfs(si, sj) - 1)
