1c13a58ca54d52af3ce95d5c2bb9ead380a16bbc
이건희 <143998370+thisis-hee@users.noreply.github.com>
2025-09-25 23:17:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 15.44 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,23 +1,25 @@
-def solution(maps):
-    n=len(maps)
-    m=len(maps[0])
-    def bfs(si,sj,ei,ej):
-        q=[]
-        v=[[0]*m for _ in range(n)]
-        v[si][sj]=1
-        q.append((si,sj))
-        
-        while q:
-            ci,cj=q.pop(0)
-            if((ci,cj)==(ei,ej)):
-                return v[ei][ej]
-            
-            for di, dj in ((-1,0),(1,0),(0,-1),(0,1)):
-                ni,nj=ci+di,cj+dj
-                if(0<=ni<n and 0<=nj<m and maps[ni][nj]==1 and v[ni][nj]==0):
-                    q.append((ni,nj))
-                    v[ni][nj]+=v[ci][cj]+1
-        return -1
+from collections import deque
+
+def bfs(si,sj,maps):
+    n,m=len(maps),len(maps[0])
+    q=deque()
+    q.append((si,sj))
+    v=[[0]*m for _ in range(n)]
+    v[si][sj]=1
     
-    answer=bfs(0,0,n-1,m-1)
+    while q:
+        ci,cj=q.popleft()
+        
+        if((ci,cj)==(n-1,m-1)):
+            return v[ci][cj]
+        for di, dj in ((-1,0),(1,0),(0,-1),(0,1)):
+            ni,nj = ci+di, cj+dj
+            if(0<=ni<n and 0<=nj<m and maps[ni][nj]==1 and v[ni][nj]==0):
+                q.append((ni,nj))
+                v[ni][nj]=v[ci][cj]+1
+    return -1
+
+
+def solution(maps):
+    answer=bfs(0,0,maps)
     return answer
