3bd6a2a5a7468ebb290f2872513cef0805a1caa4
이건희 <143998370+thisis-hee@users.noreply.github.com>
2025-09-26 20:25:34 +0900

    [level 2] Title: 짝지어 제거하기, Time: 192.19 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def solution(s):
+    stack = []
+    
+    for i in s:
+        stack.append(i)
+        
+        if(len(stack)>=2):
+            if(stack[-1]==stack[-2]):
+                stack.pop()
+                stack.pop()
+    
+    if stack:
+        return 0
+    else:
+        return 1
+
