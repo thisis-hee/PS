28358f830d531fa70aeb7a7493e0839c2b50ceb0
이건희 <143998370+thisis-hee@users.noreply.github.com>
2025-10-07 14:35:23 +0900

    [level 1] Title: 명예의 전당 (1), Time: 8.45 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,12 @@
+def solution(k, score):
+    answer = []
+    fame=[]
+
+    for i in score:
+        fame.append(i)
+        
+        if len(fame) > k:
+            fame.remove(min(fame))
+        answer.append(min(fame))
+    
+    return answer
